839649c93601c63c609c13b7473d30250b079b01
sanket bhalke <sanketbhalke1234@gmail.com>
2025-07-11 07:12:23 +0000

    updated files

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import Lenis from '@studio-freight/lenis';
+import Lenis from 'lenis';
 
 function Main() {
   useEffect(() => {
