82c8cc3ec9a8ded68703e2244250b92f2b873031
sanket bhalke <sanketbhalke1234@gmail.com>
2025-07-16 09:05:58 +0000

    updated the files

--- src/App.tsx
+++ src/App.tsx
@@ -17,15 +17,15 @@ function App() {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      orientation: 'vertical',
       smoothWheel: true,
+      smoothTouch: false,
+      touchMultiplier: 2,
     });
 
-    function raf(time: number) {
+    const raf = (time: number) => {
       lenis.raf(time);
       requestAnimationFrame(raf);
-    }
+    };
 
     requestAnimationFrame(raf);
 
